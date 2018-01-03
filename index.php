4c8595b53cd40f26cb404aac461c23e723af79dd
Alex Lee <alee490@my.bcit.ca>
2018-01-03 14:23:48 -0800

    Logo

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="HandheldFriendly" content="true">
+    <meta name="viewport" content="width=device-width, initial-scale=0.666667, maximum-scale=0.666667, user-scalable=0">
+    <meta name="viewport" content="width=device-width">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="/styles/style.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link href="/style.css" rel="stylesheet" type="text/css">
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var page = window.location.pathname;
+            page = page.substring(1);
+            document.getElementById(page).classList.add("active");
+        });
+
+    </script>
+
+    <title>BCIT Bathroom Buddy</title>
+
+</head>
+
+<body>
+    <div class="container-fluid text-center">
+       <img src="logo.png">
+        <div class="row">
+            <div class="container">
+            </div>
+        </div>
+    </div>
+    <br>
+
+</body>
+
+</html>
