6ff89e78af5f9319319f95cfc5c126ec2149b484
Alex Lee <alee490@my.bcit.ca>
2018-01-03 14:40:41 -0800

    Index update

--- index.php
+++ index.php
@@ -7,27 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=0.666667, maximum-scale=0.666667, user-scalable=0">
     <meta name="viewport" content="width=device-width">
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <link rel="stylesheet" href="/styles/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
     <link href="/style.css" rel="stylesheet" type="text/css">
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-            var page = window.location.pathname;
-            page = page.substring(1);
-            document.getElementById(page).classList.add("active");
-        });
-
-    </script>
 
     <title>BCIT Bathroom Buddy</title>
 
@@ -35,14 +21,17 @@
 
 <body>
     <div class="container-fluid text-center">
-       <img src="logo.png">
-        <div class="row">
-            <div class="container">
+        <img src="logo.png">
+
+        <div class="card">
+            <h3 class="card-header">Featured Bathroom</h3>
+            <div class="card-block">
+                <h4 class="card-title">Cleanest bathroom of the month</h4>
+                <p class="card-text">SE2 - First floor</p>
+                <a href="#" class="btn btn-primary">Go there now!</a>
             </div>
         </div>
     </div>
-    <br>
-
 </body>
 
 </html>
