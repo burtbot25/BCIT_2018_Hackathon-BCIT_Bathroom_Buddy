963bed22f0fc069631ac2ad0acd740b5c10b039e
Alex Lee <alee490@my.bcit.ca>
2018-01-03 15:31:29 -0800

    Layout fix

--- index.php
+++ index.php
@@ -2,169 +2,168 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>BCIT Bathroom Buds</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="bootstrap.css" type="text/css">
-  <link rel="stylesheet" href="style.css" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BCIT Bathroom Buds</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
 
 
 </head>
 
 <body>
-  <div class="container">
-    <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+    <div class="container-fluid text-center">
+        <img src="logo.png">
+
+
+        <div class="card">
+            <h3 class="card-header">Featured Bathroom</h3>
+            <div class="card-block">
+                <h4 class="card-title">Cleanest bathroom of the month</h4>
+                <p class="card-text">SE2 - First floor</p>
+                <a href="#" class="btn btn-primary">Go there now!</a>
+                
+            </div>
+            <br />
+        </div>
+
+<br />
+        <div class="container">
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     Dropdown button
   </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
-    <div class="bathroomcards">
-      <div class="row">
-        <div class="col-md-4">
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="#" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">SW01 First Floor</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
             </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Overall</li>
-              <li class="list-group-item">Smell</li>
-              <li class="list-group-item">Cleanliness</li>
-              <li class="list-group-item">Traffic</li>
+            <br />
 
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+            <div class="card-deck">
+                <div class="card">
+                    <img class="card-img-top" src="bathroom/bathroom1.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">Tim Horton's SE9</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Overall</li>
+                            <li class="list-group-item">Smell</li>
+                            <li class="list-group-item">Cleanliness</li>
+                            <li class="list-group-item">Traffic</li>
+
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img class="card-img-top" src="bathroom/bathroom2.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">NE1 - 209</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Overall</li>
+                            <li class="list-group-item">Smell</li>
+                            <li class="list-group-item">Cleanliness</li>
+                            <li class="list-group-item">Traffic</li>
+
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="bathroom/bathroom3.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">SW1 - 1039</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Overall</li>
+                            <li class="list-group-item">Smell</li>
+                            <li class="list-group-item">Cleanliness</li>
+                            <li class="list-group-item">Traffic</li>
+
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
             </div>
-          </div>
+
+            <!-- Second card row -->
+            <br />
+
+            <div class="card-deck">
+                <div class="card">
+                    <img class="card-img-top" src="bathroom/bathroom4.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">SW9 - 2002</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Overall</li>
+                            <li class="list-group-item">Smell</li>
+                            <li class="list-group-item">Cleanliness</li>
+                            <li class="list-group-item">Traffic</li>
+
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img class="card-img-top" src="bathroom/bathroom5.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">SE9 - 202</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Overall</li>
+                            <li class="list-group-item">Smell</li>
+                            <li class="list-group-item">Cleanliness</li>
+                            <li class="list-group-item">Traffic</li>
+
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="bathroom/bathroom6.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">SE12 - 1001</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Overall</li>
+                            <li class="list-group-item">Smell</li>
+                            <li class="list-group-item">Cleanliness</li>
+                            <li class="list-group-item">Traffic</li>
+
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+            </div>
+
+            <br />
         </div>
 
-
-        <div class="col-md-4">
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="#" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">SW01 First Floor</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Overall</li>
-              <li class="list-group-item">Smell</li>
-              <li class="list-group-item">Cleanliness</li>
-              <li class="list-group-item">Traffic</li>
-
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="#" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">SW01 First Floor</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Overall</li>
-              <li class="list-group-item">Smell</li>
-              <li class="list-group-item">Cleanliness</li>
-              <li class="list-group-item">Traffic</li>
-
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
-        </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="#" alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">SW01 First Floor</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Overall</li>
-                <li class="list-group-item">Smell</li>
-                <li class="list-group-item">Cleanliness</li>
-                <li class="list-group-item">Traffic</li>
-
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div>
-          </div>
-
-
-          <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="#" alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">SW01 First Floor</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Overall</li>
-                <li class="list-group-item">Smell</li>
-                <li class="list-group-item">Cleanliness</li>
-                <li class="list-group-item">Traffic</li>
-
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="#" alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">SW01 First Floor</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Overall</li>
-                <li class="list-group-item">Smell</li>
-                <li class="list-group-item">Cleanliness</li>
-                <li class="list-group-item">Traffic</li>
-
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div>
-          </div>
-          </div>
-
-</div>
-</div>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
 </body>
 
 </html>
